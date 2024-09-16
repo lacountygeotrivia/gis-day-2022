94cd39a863ac21917867d0222bc4a14dc610b59b
lacountygeotrivia <115057354+lacountygeotrivia@users.noreply.github.com>
2024-09-16 13:13:14 -0700

    Update Instructions.js

--- src/components/Instructions.js
+++ src/components/Instructions.js
@@ -20,7 +20,7 @@ export const Instructions = ({onDismiss:dismiss}) => {
             style={{zIndex: 2000, backgroundColor: "rgba(0,0,0,0.6)"}}>
             <div className="card align-self-center" style={{width: "80%", maxWidth: "500px"}}>
                 <div className="card-header d-flex justify-content-between">
-                    <span>Instructions</span>
+                    <span>Please Read</span>
                     <button type="button" className="btn-close" aria-label="Close" onClick={()=>dismiss()}></button>
                 </div>
                 <div className="card-body">
